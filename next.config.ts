8539de333989caed608acfc6639a4c7f4f8cdb7b
Paul Popus <paul@payloadcms.com>
2026-05-06 14:28:33 -0600

    bump to next 16.2

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
   // Packages with Cloudflare Workers (workerd) specific code
   // Read more: https://opennext.js.org/cloudflare/howtos/workerd
-  serverExternalPackages: ['jose', 'pg-cloudflare'],
+  serverExternalPackages: ['jose', 'pg-cloudflare', 'drizzle-kit'],
 
   // Your Next.js config here
   webpack: (webpackConfig: any) => {
